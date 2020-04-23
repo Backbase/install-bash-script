1b3fe5050b868a148372dfa48f3bcfd3f95ab372
Raffaele Briganti <raffaele@backbase.com>
2020-04-23 17:26:53 +0200

    Scripts improvement
    In all 3 scripts BACK_HOME is BACKBASE_HOME
    https link for ActiveMQ
    Removed useless comments
    Changed style and formatting
    Fixed the unbound variable portal error at line 297

--- unix/startcx6.sh
+++ unix/startcx6.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export BACK_HOME="/tmp/opt/backbase"
+export BACKBASE_HOME="/tmp/opt/backbase"
 
-bash $BACK_HOME/runtimes/activemq/bin/activemq start
-bash $BACK_HOME/runtimes/tomcat/platform/startup.sh && $BACK_HOME/runtimes/tomcat/portal/startup.sh && $BACK_HOME/runtimes/tomcat/editorial/startup.sh
+bash $BACKBASE_HOME/runtimes/activemq/bin/activemq start
+bash $BACKBASE_HOME/runtimes/tomcat/platform/startup.sh && $BACKBASE_HOME/runtimes/tomcat/portal/startup.sh && $BACKBASE_HOME/runtimes/tomcat/editorial/startup.sh
